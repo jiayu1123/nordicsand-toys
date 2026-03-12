5fe8b127ea4967de942209de9e000516c92d19cf
邹佳宇 <jiayuzou1123@gmail.com>
2026-03-12 22:47:03 +0000

    File changes

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -19,8 +19,8 @@ export default function ProductDetail() {
   const { data: product, isLoading } = useQuery({
     queryKey: ["product", productId],
     queryFn: async () => {
-      const products = await base44.entities.Product.filter({ id: productId });
-      return products[0] || null;
+      const all = await base44.entities.Product.list();
+      return all.find((p) => p.id === productId) || null;
     },
     enabled: !!productId,
   });
